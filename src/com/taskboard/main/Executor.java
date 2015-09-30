f4ecf48f604f63eaf03506574bea12e41cf308d4
Amarparkash Singh <nsjkapksm@gmail.com>
2015-09-30 22:00:02 +0800

    Updated Executor Class to provide support for adding floating task and recognizing invalid command type.

--- src/com/taskboard/main/Executor.java
+++ src/com/taskboard/main/Executor.java
@@ -1,51 +1,90 @@
-package com.taskboard.main;
+ package com.taskboard.main;
 
 import java.util.ArrayList;
 
 public class Executor {
 		
+	private static final int PARAM_POSITION_TASKNAME_FOR_ADD_FLOATING = 0;
+	
 	private Storage targetStorage;
 
 	public Executor(String fileName) {
 		targetStorage = new Storage(fileName);
 	}
 		
-	public void processCommand(String userInput) {
+	public Response processCommand(String userInput) {
 		Command commandInput = new Command(userInput);
 		CommandType commandType = commandInput.getCommandType();
-			
+		
+		Response responseForOperations = new Response();
+		
 		switch (commandType) {
 			case ADD:
-				executeAddCommand(commandInput);
+				responseForOperations = executeAddCommand(commandInput);
 				break;
 			case EDIT:
-				executeEditCommand();
+				responseForOperations = executeEditCommand(commandInput);
 				break;
 			case DELETE:
-				executeDeleteCommand();
+				responseForOperations = executeDeleteCommand(commandInput);
 				break;
 			case UNKNOWN:
-				return;
+				responseForOperations = failToRecogniseCommand();
+				break;
 		}
-			
+		
+		return responseForOperations; 
 	}
 	
-	public void executeAddCommand(Command commandInput) {
+	private Response executeAddCommand(Command commandInput) {
 		ArrayList<Parameter> parameters = commandInput.getParameters();
+		
+		Response responseForAdd = new Response();
+		
 		if (parameters.size() == 1) {
-			addFloatingTask();
+			responseForAdd = addFloatingTask(parameters);
 		}
+		
+		return responseForAdd;
 	}
 		
-	public void addFloatingTask() {
-	
+	private Response addFloatingTask(ArrayList<Parameter> parameters) {
+		String userFeedback = getFeedbackForAdd(parameters);
+		
+		Response responseForAddFloating = new Response();
+		responseForAddFloating.setIsSuccess(true);
+		responseForAddFloating.setFeedback(userFeedback);
+		
+		return responseForAddFloating;
 	}
 	
-	private void executeEditCommand() {
+	private String getFeedbackForAdd(ArrayList<Parameter> parameters) {
+		String taskName = parameters.get(PARAM_POSITION_TASKNAME_FOR_ADD_FLOATING).getParameterValue();
+		String userFeedback = taskName.concat(" added!");
 		
+		return userFeedback;
+	}
+	
+	private Response executeEditCommand(Command commandInput) {
+		// STUB
+		Response responseForEdit = new Response();
+		
+		return responseForEdit;
 	}
 
-	private void executeDeleteCommand() {
+	private Response executeDeleteCommand(Command commandInput) {
+		// STUB
+		Response responseForDelete = new Response();
 		
+		return responseForDelete;
+	}
+	
+	private Response failToRecogniseCommand() {
+		Response responseForInvalidCommand = new Response();
+		responseForInvalidCommand.setIsSuccess(false);
+		IllegalArgumentException exobj = new IllegalArgumentException("Invalid command type provided.");
+		responseForInvalidCommand.setIllegalArgumentException(exobj);
+		
+		return responseForInvalidCommand;
 	}
 }
