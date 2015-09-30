eee347416f25c9b67a2c413c93099a8d343ba1a0
Alvian Prasetya <alvian.better@gmail.com>
2015-10-01 00:06:15 +0800

    Implemented test driver program.

---
+++ src/com/taskboard/main/TestDriver.java
@@ -0,0 +1,18 @@
+package com.taskboard.main;
+
+public class TestDriver {
+	public static void main(String[] args) {
+		UserInterface _userInterface = new UserInterface();
+		Executor _executor = new Executor();
+		String currentCommand = _userInterface.getCommandReader().getNextCommand();
+		while (!currentCommand.toLowerCase().equals("exit")) {
+			Response currentResponse = _executor.processCommand(currentCommand);
+			if (currentResponse.getIsSuccess()) {
+				System.out.println(currentResponse.getFeedback());
+			} else {
+				currentResponse.getException().printStackTrace();
+			}
+			currentCommand = _userInterface.getCommandReader().getNextCommand();
+		}
+	}
+}
