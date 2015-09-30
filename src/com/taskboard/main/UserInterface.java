4c3d09eb1f3fb9e52bc512144329beb41afe8873
Alvian Prasetya <alvian.better@gmail.com>
2015-09-30 20:47:00 +0800

    Implemented UserInterface class.

---
+++ src/com/taskboard/main/UserInterface.java
@@ -0,0 +1,19 @@
+package com.taskboard.main;
+
+public class UserInterface {
+
+	private CommandReader _commandReader;
+	private DisplayBoard _displayBoard;
+	
+	public UserInterface() {
+		_commandReader = new CommandReader();
+		_displayBoard = new DisplayBoard();
+	}
+	
+	// accessors
+	
+	public CommandReader getCommandReader() {
+		return _commandReader;
+	}
+	
+}
