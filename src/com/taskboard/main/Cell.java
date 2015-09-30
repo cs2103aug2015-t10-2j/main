bc4b14f94d44ed9323680c8d2e5ae3643907b201
Alvian Prasetya <alvian.better@gmail.com>
2015-09-30 19:41:16 +0800

    Implemented Cell, Row, and Table classes for UI.

---
+++ src/com/taskboard/main/Cell.java
@@ -0,0 +1,31 @@
+package com.taskboard.main;
+
+public class Cell {
+
+	// attributes
+	
+	private String _content;
+	
+	// constructors
+	
+	public Cell() {
+		_content = new String();
+	}
+	
+	public Cell(String content) {
+		_content = new String(content);
+	}
+	
+	// accessors
+	
+	public String getContent() {
+		return _content;
+	}
+	
+	// mutators
+	
+	public void setContent(String newContent) {
+		_content = newContent;
+	}
+	
+}
---
+++ src/com/taskboard/main/Row.java
@@ -0,0 +1,53 @@
+package com.taskboard.main;
+
+import java.util.ArrayList;
+
+public class Row {
+
+	// attributes
+	
+	private ArrayList<Cell> _cells;
+	
+	// constructors
+	
+	public Row() {
+		_cells = new ArrayList<Cell>();
+	}
+	
+	public Row(ArrayList<Cell> cells) {
+		_cells = cells;
+	}
+	
+	// accessors
+	
+	public int getNumCells() {
+		return _cells.size();
+	}
+	
+	public ArrayList<Cell> getCells() {
+		return _cells;
+	}
+	
+	public Cell getCell(int id) {
+		return _cells.get(id);
+	}
+	
+	// mutators
+	
+	public void addCells(ArrayList<Cell> newCells) {
+		_cells.addAll(newCells);
+	}
+	
+	public void addCell(Cell newCell) {
+		_cells.add(newCell);
+	}
+	
+	public void setCells(ArrayList<Cell> newCells) {
+		_cells = newCells;
+	}
+	
+	public void setCell(int id, Cell newCell) {
+		_cells.set(id, newCell);
+	}
+	
+}
