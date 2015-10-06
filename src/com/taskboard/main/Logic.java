dc5c67dce8324210239b20c2ba1da7139c4151aa
Amarparkash Singh <nsjkapksm@gmail.com>
2015-10-07 02:40:37 +0800

    Added methods for deadline task.

--- src/com/taskboard/main/Logic.java
+++ src/com/taskboard/main/Logic.java
@@ -9,9 +9,15 @@ public class Logic {
 	private static final String MESSAGE_WELCOME = "Welcome to TASKBOARD!"; 
 	private static final String MESSAGE_AFTER_ADD = "\"%1$s\" added!";
 	private static final String MESSAGE_AFTER_DELETE = "\"%1$s\" deleted!";
+	private static final String MESSAGE_ERROR_FOR_ADD = "The entry could not be added to the file.";
+	private static final String MESSAGE_ERROR_FOR_LAUNCH = "Fail to create new scheduler.";
+	private static final String MESSAGE_ERROR_INVALID_TIME = "Invalid time provided.";
+	private static final String MESSAGE_ERROR_FOR_DELETE = "The entry could not be deleted from the file.";
+	private static final String MESSAGE_ERROR_INVALID_COMMAND = "Invalid command type provided.";
 	
 	private static final int INDEX_OF_TASKNAME_FOR_ADD_FLOATING = 0;
 	private static final int INDEX_OF_FILENAME_FOR_LAUNCH = 0;
+	private static final int INDEX_OF_FIRST_CHAR_IN_TIME = 0;
 	
 	// attribute
 	
@@ -66,16 +72,24 @@ public class Logic {
 		Response responseForLaunch = new Response();
 		
 		if (_storageHandler.isSetUpSuccessful(fileName)) {
-			responseForLaunch.setIsSuccess(true);
-			String userFeedback = getFeedbackForUser(MESSAGE_WELCOME, fileName);
-			responseForLaunch.setFeedback(userFeedback);
+			setSuccessResponseForLaunch(responseForLaunch, fileName);
 		} else {
-			responseForLaunch.setIsSuccess(false);
-			IOException exobj = new IOException("Fail to create new scheduler.");
-			responseForLaunch.setException(exobj);
+			setFailureResponseForLaunch(responseForLaunch);
 		}	
 		
 		return responseForLaunch;
+	}
+	
+	private void setSuccessResponseForLaunch(Response response, String fileName) {
+		response.setIsSuccess(true);
+		String userFeedback = getFeedbackForUser(MESSAGE_WELCOME, fileName);
+		response.setFeedback(userFeedback);
+	}
+	
+	private void setFailureResponseForLaunch(Response response) {
+		response.setIsSuccess(false);
+		IOException exobj = new IOException(MESSAGE_ERROR_FOR_LAUNCH);
+		response.setException(exobj);
 	}
 	
 	private String getFeedbackForUser(String feedbackMessage, String details) {
@@ -87,31 +101,49 @@ public class Logic {
 		
 		Response responseForAdd = new Response();
 		
-		if (parameters.size() == 1) {
-			Parameter taskNameParameter = parameters.get(INDEX_OF_TASKNAME_FOR_ADD_FLOATING);
-			String taskName = taskNameParameter.getParameterValue();
-			responseForAdd = addFloatingTask(taskName);
+		if (isAddFloatingTask(parameters)) {
+			responseForAdd = addFloatingTask(parameters);
+		} else if (isAddDeadlineTask(parameters)) {
+			responseForAdd = addDeadlineTask(parameters);
 		}
 		
 		return responseForAdd;
 	}
+	
+	private boolean isAddFloatingTask (ArrayList<Parameter> parameters) {
+		if (parameters.size() == 1) {
+			return true;
+		}
 		
-	private Response addFloatingTask(String taskName) {		
+		return false;
+	}
+		
+	private Response addFloatingTask(ArrayList<Parameter> parameters) {		
+		Parameter taskNameParameter = parameters.get(INDEX_OF_TASKNAME_FOR_ADD_FLOATING);
+		String taskName = taskNameParameter.getParameterValue();
 		Entry floatingTask = formatFloatingTaskForStorage(taskName);
 		
 		Response responseForAddFloating = new Response();
 		
 		if (_storageHandler.isAddToFileSuccessful(floatingTask)) {
-			responseForAddFloating.setIsSuccess(true);
-			String userFeedback = getFeedbackForUser(MESSAGE_AFTER_ADD, taskName);
-			responseForAddFloating.setFeedback(userFeedback);
+			setSuccessResponseForAddFloating(responseForAddFloating, taskName);
 		} else {
-			responseForAddFloating.setIsSuccess(false);
-			IOException exobj = new IOException("The entry could not be added to the file.");
-			responseForAddFloating.setException(exobj);
+			setFailureResponseForAddFloating(responseForAddFloating);
 		}
 		
 		return responseForAddFloating;
+	}
+	
+	private void setSuccessResponseForAddFloating(Response response, String taskName) {
+		response.setIsSuccess(true);
+		String userFeedback = getFeedbackForUser(MESSAGE_AFTER_ADD, taskName);
+		response.setFeedback(userFeedback);
+	}
+	
+	private void setFailureResponseForAddFloating(Response response) {
+		response.setIsSuccess(false);
+		IOException exobj = new IOException(MESSAGE_ERROR_FOR_ADD);
+		response.setException(exobj);
 	}
 	
 	private Entry formatFloatingTaskForStorage(String taskName) {
@@ -122,6 +154,82 @@ public class Logic {
 		//floatingTask.addToDetails("\n");
 		
 		return floatingTask;
+	}
+	
+	private boolean isAddDeadlineTask(ArrayList<Parameter> parameters) {
+		for (int i = 0; i < parameters.size(); i++) {
+			Parameter parameter = parameters.get(i);
+			ParameterType parameterType = parameter.getParameterType();
+			
+			if (parameterType == ParameterType.DATE || parameterType == ParameterType.TIME) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	private Response addDeadlineTask(ArrayList<Parameter> parameters) {
+		Response responseForAddDeadline = new Response();
+		
+		for (int i = 0; i < parameters.size(); i++) {
+			Parameter parameter = parameters.get(i);
+			ParameterType parameterType = parameter.getParameterType();
+			
+			if (parameterType == ParameterType.TIME) {
+				String time = parameter.getParameterValue();
+			} else if (parameterType == ParameterType.DATE) {
+				String date = parameter.getParameterValue();
+			} else {
+				String taskName = parameter.getParameterValue();
+			}
+		}
+		
+//		if (!isDateValid(date)) {
+//			
+//		} else if (!isTimeValid(time)) {
+//			setFailureResponseForInvalidTime(responseForAddDeadline);
+//		} else {
+//			
+//		}
+		
+		return responseForAddDeadline;
+	}
+	
+	private boolean isDateValid(ArrayList<Parameter> parameters) {
+		// Stub
+		return false;
+	}
+	
+	private boolean isTimeValid(String time) {
+		int indexOfColon = time.indexOf(':');
+		String hourPart = time.substring(INDEX_OF_FIRST_CHAR_IN_TIME, indexOfColon);
+		int hour = Integer.parseInt(hourPart);
+		String minutePart = time.substring(indexOfColon + 1);
+		int minute = Integer.parseInt(minutePart);
+				
+		boolean isHourValid = true;
+		boolean isMinuteValid = true;
+				
+		if (hour < 0 || hour > 23) {
+			isHourValid = false;
+		}
+				
+		if (minute < 0 || minute > 59) {
+			isMinuteValid = false;
+		}
+				
+		if (!isHourValid || !isMinuteValid) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private void setFailureResponseForInvalidTime(Response response) {
+		response.setIsSuccess(false);
+		IllegalArgumentException exobj = new IllegalArgumentException(MESSAGE_ERROR_INVALID_TIME);
+		response.setException(exobj);
 	}
 	
 	private Response executeEditCommand(Command commandInput) {
@@ -154,16 +262,24 @@ public class Logic {
 		String taskName = parameter.getParameterValue();
 		
 		if (_storageHandler.isDeletingSuccessful(taskName)) {
-			responseForDeleteByName.setIsSuccess(true);
-			String userFeedback = getFeedbackForUser(MESSAGE_AFTER_DELETE, taskName);
-			responseForDeleteByName.setFeedback(userFeedback);
+			setSuccessResponseForDeleteByName(responseForDeleteByName, taskName);
 		} else {
-			responseForDeleteByName.setIsSuccess(false);
-			IOException exobj = new IOException("The entry could not be deleted from the file.");
-			responseForDeleteByName.setException(exobj);
+			setFailureResponseForDeleteByName(responseForDeleteByName);
 		}
 		
 		return responseForDeleteByName;
+	}
+	
+	private void setSuccessResponseForDeleteByName(Response response, String taskName) {
+		response.setIsSuccess(true);
+		String userFeedback = getFeedbackForUser(MESSAGE_AFTER_DELETE, taskName);
+		response.setFeedback(userFeedback);
+	}
+	
+	private void setFailureResponseForDeleteByName(Response response) {
+		response.setIsSuccess(false);
+		IOException exobj = new IOException(MESSAGE_ERROR_FOR_DELETE);
+		response.setException(exobj);
 	}
 	
 	public String retrieveEntries() {
@@ -175,7 +291,7 @@ public class Logic {
 	private Response failToRecogniseCommand() {
 		Response responseForInvalidCommand = new Response();
 		responseForInvalidCommand.setIsSuccess(false);
-		IllegalArgumentException exobj = new IllegalArgumentException("Invalid command type provided.");
+		IllegalArgumentException exobj = new IllegalArgumentException(MESSAGE_ERROR_INVALID_COMMAND);
 		responseForInvalidCommand.setException(exobj);
 		
 		return responseForInvalidCommand;
