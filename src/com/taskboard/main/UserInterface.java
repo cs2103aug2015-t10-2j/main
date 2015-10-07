6f04cad10f7e0628c1ecd14a8316c9aba8e6cf49
ChrisRaynaldo <chris.raynaldo@gmail.com>
2015-10-07 15:31:36 +0800

    Added main method to UserInterface class
    Added to UserInterface:
    -main method
    -Logic and Response attributes
    
    To read user inputs and return feedback to users

--- src/com/taskboard/main/UserInterface.java
+++ src/com/taskboard/main/UserInterface.java
@@ -4,16 +4,41 @@ public class UserInterface {
 
 	private CommandReader _commandReader;
 	private DisplayBoard _displayBoard;
+	private Logic _logic;
+	private Response _response;
 	
 	public UserInterface() {
 		_commandReader = new CommandReader();
 		_displayBoard = new DisplayBoard();
+		_logic = new Logic();
+		_response = new Response();
+	}
+	
+	public static void main(String[] args){
+		String userInput;
+		String feedback;
+		
+		UserInterface _userInterface = new UserInterface();
+		userInput = _userInterface.getCommandReader().getNextCommand();	
+		
+		_userInterface.getLogic().processCommand(userInput);
+		
+		feedback = _userInterface.getResponse().getFeedback();
+		_userInterface.getCommandReader().showToUser(feedback);
 	}
 	
 	// accessors
 	
 	public CommandReader getCommandReader() {
 		return _commandReader;
+	}
+	
+	public Logic getLogic() {
+		return _logic;
+	}
+	
+	public Response getResponse() {
+		return _response;
 	}
 	
 	public DisplayBoard getDisplayBoard() {
