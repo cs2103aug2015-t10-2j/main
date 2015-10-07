03e279b7c3fd11e16a54a2a387dc035e4c5a7eba
Alvian Prasetya <alvian.better@gmail.com>
2015-10-07 19:27:56 +0800

    Extracted methods from Command class to a new class CommandTypeParser.

---
+++ src/com/taskboard/main/CommandTypeParser.java
@@ -0,0 +1,44 @@
+package com.taskboard.main;
+
+public class CommandTypeParser {
+
+	public CommandTypeParser() {
+		
+	}
+	
+	public CommandType parseCommandType(String commandString) {
+		String commandTypeString = extractCommandTypeString(commandString).toLowerCase();
+		
+		switch (commandTypeString) {
+			case "a":
+			case "add":
+				return CommandType.ADD;
+			case "e":
+			case "edit":
+				return CommandType.EDIT;
+			case "d":
+			case "delete":
+				return CommandType.DELETE;
+			case "o":
+			case "open":
+				return CommandType.OPEN;
+			case "n":
+			case "new":
+				return CommandType.NEW;
+			case "esc":
+			case "exit":
+				return CommandType.EXIT;
+			default:
+				return CommandType.UNKNOWN;
+		}
+	}
+	
+	public static String extractCommandTypeString(String commandString) {
+		if (commandString.indexOf(' ') == -1) {
+			return commandString;
+		} else {
+			return commandString.substring(0, commandString.indexOf(' ')).toLowerCase();
+		}
+	}
+	
+}
