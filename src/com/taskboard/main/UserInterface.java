d5fe5ea2b6dd90de6ef4794b1601033af046d0eb
Alvian Prasetya <alvian.better@gmail.com>
2015-10-14 20:23:17 +0800

    Implemented a (very) basic graphical user interface. Using Javax.Swing

--- src/com/taskboard/main/UserInterface.java
+++ src/com/taskboard/main/UserInterface.java
@@ -1,87 +1,164 @@
 package com.taskboard.main;
 
-public class UserInterface {
+import javax.swing.*;
 
-	private CommandReader _commandReader;
-	private DisplayBoard _displayBoard;
+import java.awt.*;
+import java.awt.event.*;
+
+public class UserInterface extends JFrame {
+
+	private static final long serialVersionUID = 1;
+	
 	private Logic _logic;
-	private Response _response;
+	
+	private JLabel _title;
+	private JTextArea _displayArea;
+	private JTextArea _feedbackArea;
+	private JLabel _commandLabel;
+	private JTextField _commandField;
+	private JButton _submitButton;
 	
 	public UserInterface() {
-		_commandReader = new CommandReader();
-		_displayBoard = new DisplayBoard();
-		_logic = new Logic();
-		_response = new Response();
-	}
-	
-	public static void main(String[] args){
-		UserInterface _userInterface = new UserInterface();
-		_userInterface.initializeDisplayBoard();
-		
-		String userInput = _userInterface.getCommandReader().getNextCommand();	
-		while (!userInput.toLowerCase().equals("exit")) {
-			Response currentResponse = _userInterface.getLogic().processCommand(userInput);
-			if (currentResponse.getIsSuccess()) {
-				String feedback = currentResponse.getFeedback().trim();
-				if (userInput.toLowerCase().equals("view")) {
-					_userInterface.getDisplayBoard().getTable(0).getRow(0).getCell(0).setContent(feedback);
-					_userInterface.getDisplayBoard().getTable(1).getRow(0).getCell(0).setContent("Successfully displayed all entries.");
-				} else {
-					_userInterface.getDisplayBoard().getTable(0).getRow(0).getCell(0).setContent("No entry to display.");
-					_userInterface.getDisplayBoard().getTable(1).getRow(0).getCell(0).setContent(feedback);
-				}
-			} else {
-				String exception = currentResponse.getException().getMessage();
-				_userInterface.getDisplayBoard().getTable(1).getRow(0).getCell(0).setContent(exception);
-			}
-			clearScreen();
-			_userInterface.getDisplayBoard().printDisplayBoard();
-			
-			userInput = _userInterface.getCommandReader().getNextCommand();
-		}
-	}
-	
-	// accessors
-	
-	public CommandReader getCommandReader() {
-		return _commandReader;
+		initComponents();
 	}
 	
 	public Logic getLogic() {
 		return _logic;
 	}
 	
-	public Response getResponse() {
-		return _response;
-	}
-	
-	public DisplayBoard getDisplayBoard() {
-		return _displayBoard;
-	}
-	
-	// functionalities
-	
-	public void initializeDisplayBoard() {
-		Cell displayCell = new Cell();
-		Row displayRow = new Row();
-		displayRow.addCell(displayCell);
-		Table displayTable = new Table();
-		displayTable.addRow(displayRow);
+	private void initComponents() {
+		_logic = new Logic();
 		
-		Cell responseCell = new Cell();
-		Row responseRow = new Row();
-		responseRow.addCell(responseCell);
-		Table responseTable = new Table();
-		responseTable.addRow(responseRow);
+		_title = new JLabel("TaskBoard V0.1");
+		_title.setFont(new Font("Serif", Font.BOLD, 28));
 		
-		_displayBoard.addTable(displayTable);
-		_displayBoard.addTable(responseTable);
+		_displayArea = new JTextArea(20, 50);
+		_displayArea.setEditable(false);
+		
+		_feedbackArea = new JTextArea(1, 50);
+		_feedbackArea.setEditable(false);
+		
+		_commandLabel = new JLabel("Enter command below:");
+		
+		_commandField = new JTextField(50);
+		
+		_commandField.addKeyListener(new KeyListener() {
+			@Override
+            public void keyTyped(KeyEvent arg0) {
+                // TODO Auto-generated method stub  
+            }
+
+            @Override
+            public void keyReleased(KeyEvent arg0) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void keyPressed(KeyEvent arg0) {
+            	if (arg0.getKeyCode() == KeyEvent.VK_ENTER){
+            		String userInput = _commandField.getText();
+                	
+                	if (userInput.toLowerCase().equals("exit")) {
+                		System.exit(0);
+                	} else {
+                		Response currentResponse = getLogic().processCommand(userInput);
+                		if (currentResponse.getIsSuccess()) {
+                			String feedback = currentResponse.getFeedback().trim();
+                			if (userInput.toLowerCase().equals("view")) {
+                				_displayArea.setText(feedback);
+                				_feedbackArea.setText(new String("Successfully displayed all entries."));
+                			} else {
+                				_displayArea.setText(new String("No entry to display"));
+                				_feedbackArea.setText(feedback);
+                			}
+                		}
+                	}
+                	
+                    _commandField.setText("");
+                }
+            }
+        });
+		
+		_submitButton = new JButton("Submit");
+		
+		_submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+            	String userInput = _commandField.getText();
+            	
+            	if (userInput.toLowerCase().equals("exit")) {
+            		System.exit(0);
+            	} else {
+            		Response currentResponse = getLogic().processCommand(userInput);
+            		if (currentResponse.getIsSuccess()) {
+            			String feedback = currentResponse.getFeedback().trim();
+            			if (userInput.toLowerCase().equals("view")) {
+            				_displayArea.setText(feedback);
+            				_feedbackArea.setText(new String("Successfully displayed all entries."));
+            			} else {
+            				_displayArea.setText(new String("No entry to display"));
+            				_feedbackArea.setText(feedback);
+            			}
+            		}
+            	}
+            	
+                _commandField.setText("");
+            }
+        });
+		
+		createLayout(new JComponent[]{_title, _displayArea, _feedbackArea, _commandLabel, _commandField, _submitButton});
+		_commandField.requestFocusInWindow();
+		
+		setTitle("TaskBoard: The Revolutionary Task Manager");
+		//setSize(800, 600);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
-	public static void clearScreen() {
-		for (int clear = 0; clear < 1000; clear++) {
-		    System.out.println("\b") ;
-		}
+	private void createLayout(JComponent... arg) {
+
+        Container pane = getContentPane();
+        GroupLayout gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        gl.setAutoCreateContainerGaps(true);
+        
+	    gl.setHorizontalGroup(gl.createSequentialGroup()
+	    		.addGroup(gl.createParallelGroup()
+	    			.addComponent(arg[0])
+	    			.addComponent(arg[1])
+	    			.addComponent(arg[2])
+	    			.addComponent(arg[3])
+	    			.addComponent(arg[4])
+	    			.addComponent(arg[5])
+		    	)
+	    );
+	
+	    gl.setVerticalGroup(gl.createSequentialGroup()
+	    		.addComponent(arg[0])
+	    		.addGap(10)
+			    .addComponent(arg[1])
+			    .addGap(10)
+			    .addComponent(arg[2])
+			    .addGap(10)
+			    .addComponent(arg[3])
+			    .addGap(10)
+			    .addComponent(arg[4])
+			    .addGap(10)
+			    .addComponent(arg[5])
+	    );
+        
+	    pack();
+    }
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				UserInterface _userInterface = new UserInterface();
+				_userInterface.setVisible(true);
+			}
+		});
 	}
 	
 }
