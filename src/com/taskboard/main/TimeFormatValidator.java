9305d81b176b39ec6f7e5d9e6bd3ea91a4ab84f6
Alvian Prasetya <alvian.better@gmail.com>
2015-10-16 14:45:04 +0800

    Fixed 12am/12pm bug of TimeFormatValidator class. 12am should be equivalent to 00:00. 12pm should be equivalent to 12:00.

--- src/com/taskboard/main/TimeFormatValidator.java
+++ src/com/taskboard/main/TimeFormatValidator.java
@@ -20,9 +20,17 @@ public class TimeFormatValidator implements FormatValidator {
 		String mm = new String();
 		if (token.indexOf("am") != -1) {
 			hh = token.substring(0, token.indexOf("am"));
+			// 12am is 00:00
+			if (hh.equals("12")) {
+				hh = "00";
+			}
 			mm = "00";
 		} else if (token.indexOf("pm") != -1) {
 			hh = Integer.toString(Integer.parseInt(token.substring(0, token.indexOf("pm"))) + 12);
+			// 12pm is 12:00
+			if (hh.equals("24")) {
+				hh = "12";
+			}
 			mm = "00";
 		} else if (token.indexOf(":") == 2 && token.length() == 5) {
 			return token;
