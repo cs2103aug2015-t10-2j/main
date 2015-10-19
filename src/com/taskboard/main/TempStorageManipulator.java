091c519f5bca5fd191dc8f78b64a452066a1448e
Do Nguyen Dung <donguyendung.20.09.1993@gmail.com>
2015-10-19 20:15:18 +0800

    Update TSM with isEntryTypeChanged boolean

--- src/com/taskboard/main/TempStorageManipulator.java
+++ src/com/taskboard/main/TempStorageManipulator.java
@@ -7,89 +7,107 @@ import java.util.ArrayList;
 import java.util.logging.*;
 
 public class TempStorageManipulator {
-	
+
 	// attributes
 
 	private ArrayList<Entry> _tempStorage;
 	private ArrayList<Entry> _tempArchive;
 	StorageHandler _storageHandler;
 	ArchiveHandler _archiveHandler;
-		
+
 	private static Logger logger = Logger.getLogger("TempStorageManipulator");
 	// constructor
-	
+
 	public TempStorageManipulator() {
 		_storageHandler = new StorageHandler();
 		_archiveHandler = new ArchiveHandler();
 	}
-	
+
 	// accessor
-	
+
 	public ArrayList<Entry> getTempStorage() {
 		return _tempStorage;
 	}
-	
-	public void initialise(String fileName) throws IllegalArgumentException, IOException  {
+
+	public void initialise(String fileName) throws IllegalArgumentException, IOException {
 		_tempStorage = _storageHandler.createNewFile(fileName);
 		logger.log(Level.INFO, "Initialise a new temporary storage.");
 	}
-	
+
 	public void repopulate(String fileName) throws IllegalArgumentException, FileNotFoundException {
 		_tempStorage = _storageHandler.openExistingFile(fileName);
 		logger.log(Level.INFO, "Repopulate a temporary storage.");
 	}
-	
+
 	public void addToTempStorage(Entry entry) throws IOException {
 		_tempStorage.add(entry);
 		_storageHandler.updateTempStorageToFile(_tempStorage);
 		logger.log(Level.INFO, "Add entry into temp storage.");
 	}
-	
+
 	public void editTempStorage(int i, ArrayList<Parameter> newContent, boolean isEntryTypeChanged) throws IOException {
 		Entry editedEntry = _tempStorage.get(i);
 		ArrayList<Parameter> entryDetails = editedEntry.getParameters();
-		replaceWithNewContent(entryDetails, newContent);
+		replaceWithNewContent(entryDetails, newContent, isEntryTypeChanged);
 		logger.log(Level.INFO, "Replace old entries with new ones.");
 		editedEntry.setParameters(entryDetails);
 		_tempStorage.set(i, editedEntry);
 		setTempStorageToFile(_tempStorage);
 	}
-	
-	private void replaceWithNewContent(ArrayList<Parameter> entryDetails, ArrayList<Parameter> newContent) {
+
+	private void replaceWithNewContent(ArrayList<Parameter> entryDetails, ArrayList<Parameter> newContent,
+			boolean isEntryTypeChanged) {
 		boolean isDetailPresent = false;
-		
+
 		for (int i = 1; i < newContent.size(); i++) {
 			Parameter newFormattedDetail = newContent.get(i);
-//			String[] newFormattedDetailSegments = newFormattedDetail.split(":");
-			
-			for (int j = 0; j < entryDetails.size(); j++) {
-//				String existingFormattedDetail = entryDetails.get(j).getParameterValue();
-				
-				if (entryDetails.get(j).getParameterType() == newFormattedDetail.getParameterType()) {					
-					entryDetails.get(j).setParameterValue(newFormattedDetail.getParameterValue());
-					logger.log(Level.INFO, "Entry to be edited found and replaced.");
-					isDetailPresent = true;
-				} 
-			}
-			
-			if (!isDetailPresent) {
+
+			if (!isEntryTypeChanged) {
+				for (int j = 0; j < entryDetails.size(); j++) {
+					if (entryDetails.get(j).getParameterType() == newFormattedDetail.getParameterType()) {
+						entryDetails.get(j).setParameterValue(newFormattedDetail.getParameterValue());
+						logger.log(Level.INFO, "Entry to be edited found and replaced.");
+						isDetailPresent = true;
+					}
+				}
+
+				if (!isDetailPresent) {
+					Parameter newParameter = new Parameter();
+					newParameter.setParameterType(newFormattedDetail.getParameterType());
+					newParameter.setParameterValue(newFormattedDetail.getParameterValue());
+					logger.log(Level.INFO, "New parameters added into an existing entry.");
+					entryDetails.add(newParameter);
+				}
+
+				isDetailPresent = false;
+			} else {
+				assert isEntryTypeChanged: true;
+				logger.log(Level.INFO, "Type of Entry is changed.");
+				Entry tempEntry = new Entry();
+				ArrayList<Parameter> tempParameters = tempEntry.getParameters();
+				for (int k = 0; k < entryDetails.size(); k++) {
+					if ((entryDetails.get(k).getParameterType() == ParameterType.valueOf("NAME")) || 
+					(entryDetails.get(k).getParameterType() == ParameterType.valueOf("INDEX")) ||
+					(entryDetails.get(k).getParameterType() == ParameterType.valueOf("CATEGORY")) ||
+					(entryDetails.get(k).getParameterType() == ParameterType.valueOf("PRIORITY"))) {
+						tempParameters.add(entryDetails.get(k));
+					}
 				Parameter newParameter = new Parameter();
 				newParameter.setParameterType(newFormattedDetail.getParameterType());
 				newParameter.setParameterValue(newFormattedDetail.getParameterValue());
 				logger.log(Level.INFO, "New parameters added into an existing entry.");
-				entryDetails.add(newParameter);
+				tempParameters.add(newParameter);
+				}
 			}
-			
-			isDetailPresent = false;
 		}
 	}
-	
+
 	public void deleteFromTempStorage(int i) throws IOException {
 		_tempStorage.remove(i);
 		setTempStorageToFile(_tempStorage);
 		logger.log(Level.INFO, "Delete an entry from temp storage.");
 
-	}		
+	}
 
 	public void setCompletedInTempStorage(int i) throws IOException {
 		Entry entry = _tempStorage.get(i);
@@ -103,9 +121,11 @@ public class TempStorageManipulator {
 
 	public void setTempStorageToFile(ArrayList<Entry> entries) throws IOException {
 		_storageHandler.updateTempStorageToFile(entries);
+		logger.log(Level.INFO, "Transfer the temp storage into file.");
 	}
-	
+
 	public void setTempArchiveToFile(ArrayList<Entry> entries) throws IOException {
 		_archiveHandler.updateTempStorageToFile(entries);
+		logger.log(Level.INFO, "Transfer the temp storage into archive.");
 	}
 }
