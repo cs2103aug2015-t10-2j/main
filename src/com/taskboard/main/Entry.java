c8148d5ad9350be5c0b5036c4985d84c4846df74
Alvian Prasetya <alvian.better@gmail.com>
2015-10-21 16:47:34 +0800

    Added accessor and mutator for INDEX Parameter to Entry class.

--- src/com/taskboard/main/Entry.java
+++ src/com/taskboard/main/Entry.java
@@ -1,132 +1,149 @@
 package com.taskboard.main;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Entry {
-
+	
 	// attribute
-
+	
 	private ArrayList<Parameter> _parameters;
 	private boolean _isCompleted;
 	
 	// constructor
-
+	
 	public Entry() {
 		_parameters = new ArrayList<Parameter>();
 		_isCompleted = false;
 	}
-
+	
 	// accessors
-
+	
 	public ArrayList<Parameter> getParameters() {
 		return _parameters;
 	}
-
+	
+	public Parameter getIndexParameter() {
+		for (int i = 0; i < _parameters.size(); i++) {
+			if (_parameters.get(i).getParameterType() == ParameterType.INDEX) {
+				return _parameters.get(i);
+			}
+		}
+		
+		return null;
+	}
+	
 	public Parameter getNameParameter() {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.NAME) {
 				return _parameters.get(i);
 			}
 		}
-
+		
 		return null;
 	}
-
+	
 	public Parameter getDateParameter() {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.DATE) {
 				return _parameters.get(i);
 			}
 		}
-
+		
 		return null;
 	}
-
+	
 	public Parameter getTimeParameter() {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.TIME) {
 				return _parameters.get(i);
 			}
 		}
-
+		
 		return null;
 	}
-
+	
 	public Parameter getStartDateParameter() {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.START_DATE) {
 				return _parameters.get(i);
 			}
 		}
-
+		
 		return null;
 	}
-
+	
 	public Parameter getStartTimeParameter() {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.START_TIME) {
 				return _parameters.get(i);
 			}
 		}
-
+		
 		return null;
 	}
-
+	
 	public Parameter getEndDateParameter() {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.END_DATE) {
 				return _parameters.get(i);
 			}
 		}
-
+		
 		return null;
 	}
-
+	
 	public Parameter getEndTimeParameter() {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.END_TIME) {
 				return _parameters.get(i);
 			}
 		}
-
+		
 		return null;
 	}
-
+	
 	public Parameter getPriorityParameter() {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.PRIORITY) {
 				return _parameters.get(i);
 			}
 		}
-
+		
 		return null;
 	}
-
+	
 	public Parameter getCategoryParameter() {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.CATEGORY) {
 				return _parameters.get(i);
 			}
 		}
-
+		
 		return null;
 	}
-
+	
 	public boolean isCompleted() {
 		return _isCompleted;
 	}
-
+	
 	// mutators
-
+	
 	public void setParameters(ArrayList<Parameter> newParameters) {
 		_parameters = newParameters;
 	}
-
+	
 	public void addToParameters(Parameter newParameter) {
 		_parameters.add(newParameter);
 	}
-
+	
+	public void setIndexParameter(Parameter newIndexParameter) {
+		for (int i = 0; i < _parameters.size(); i++) {
+			if (_parameters.get(i).getParameterType() == ParameterType.INDEX) {
+				_parameters.set(i, newIndexParameter);
+			}
+		}
+	}
+	
 	public void setNameParameter(Parameter newNameParameter) {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.NAME) {
@@ -142,7 +159,7 @@ public class Entry {
 			}
 		}
 	}
-
+	
 	public void setTimeParameter(Parameter newTimeParameter) {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.TIME) {
@@ -150,7 +167,7 @@ public class Entry {
 			}
 		}
 	}
-
+	
 	public void setStartDateParameter(Parameter newStartDateParameter) {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.START_DATE) {
@@ -158,7 +175,7 @@ public class Entry {
 			}
 		}
 	}
-
+	
 	public void setStartTimeParameter(Parameter newStartTimeParameter) {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.START_TIME) {
@@ -166,7 +183,7 @@ public class Entry {
 			}
 		}
 	}
-
+	
 	public void setEndDateParameter(Parameter newEndDateParameter) {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.END_DATE) {
@@ -174,7 +191,7 @@ public class Entry {
 			}
 		}
 	}
-
+	
 	public void setEndTimeParameter(Parameter newEndTimeParameter) {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.END_TIME) {
@@ -182,7 +199,7 @@ public class Entry {
 			}
 		}
 	}
-
+	
 	public void setCategoryParameter(Parameter newCategoryParameter) {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.CATEGORY) {
@@ -190,7 +207,7 @@ public class Entry {
 			}
 		}
 	}
-
+	
 	public void setPriorityParameter(Parameter newPriorityParameter) {
 		for (int i = 0; i < _parameters.size(); i++) {
 			if (_parameters.get(i).getParameterType() == ParameterType.PRIORITY) {
@@ -198,22 +215,24 @@ public class Entry {
 			}
 		}
 	}
-
+	
 	public void setCompleted(boolean newIsCompleted) {
 		_isCompleted = newIsCompleted;
 	}
-
+	
 	@Override
 	public String toString() {
 		String entryDetails = "";
-
+		
 		for (int i = 0; i < _parameters.size(); i++) {
 			Parameter detailParameter = _parameters.get(i);
 			String detailType = detailParameter.getParameterType().name();
 			String detail = detailParameter.getParameterValue();
-			entryDetails = entryDetails.concat(detailType).concat(": ").concat(detail).concat("\n");
+			entryDetails = entryDetails.concat(detailType).concat(": ")
+					.concat(detail).concat("\n");
 		}
-
+		
 		return entryDetails;
 	}
+	
 }
