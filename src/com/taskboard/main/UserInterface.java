bfc3f45121777ec16857b89b7edf7314d0194430
ChrisRaynaldo <chris.raynaldo@gmail.com>
2015-10-28 16:29:04 +0800

    Updated UserInterface using GridBagLayout

--- src/com/taskboard/main/UserInterface.java
+++ src/com/taskboard/main/UserInterface.java
@@ -12,117 +12,149 @@ public class UserInterface extends JFrame {
 
 	private static final long serialVersionUID = 1;
 	private static final String TITLE = "TaskBoard: Your Revolutionary Task Manager";
-	
+
 	private Logic _logic;
-	
-	private JLabel _title;
+
 	private JTextArea _displayArea;
 	private JTextArea _feedbackArea;
 	private JLabel _commandLabel;
 	private JTextField _commandField;
-	
+	private JButton _submitButton;
+
 	private static Logger logger = Logger.getLogger("UserInterface");
-	
+
 	public UserInterface() {
-		initComponents();
+		JFrame frame = new JFrame("TaskBoard");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		initComponents(frame.getContentPane());
 	}
-	
+
 	public Logic getLogic() {
 		return _logic;
 	}
-	
-	private void initComponents() {
-		Container cp = getContentPane();
-		cp.setLayout(new FlowLayout());
-		
+
+	private void initComponents(Container pane) {
+		pane.setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+
 		_logic = new Logic();
-		
-		_title = new JLabel("TaskBoard V0.1");
-		_title.setFont(new Font("Serif", Font.BOLD, 28));
-		
+
 		_displayArea = new JTextArea(20, 50);
 		_displayArea.setEditable(false);
-		cp.add(_displayArea);
-		
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.anchor = GridBagConstraints.PAGE_START;
+		constraints.weightx = 0.0;
+		constraints.weighty = 0.5;
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		constraints.gridwidth = 2;
+		pane.add(_displayArea, constraints);
+
 		JScrollPane _displayScroll = new JScrollPane(_displayArea);
 		_displayScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		
+
 		_feedbackArea = new JTextArea(1, 50);
 		_feedbackArea.setEditable(false);
-		cp.add(_feedbackArea);
-		
+		constraints.weightx = 0.0;
+		constraints.weighty = 0.5;
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.gridx = 0;
+		constraints.gridy = 1;
+		constraints.gridwidth = 2;
+		pane.add(_feedbackArea, constraints);
+
 		_commandLabel = new JLabel("Enter command below:");
-		cp.add(_commandLabel);
-		
-		_commandField = new JTextField(50);
-		
+		_commandLabel.setFont(new Font("Sans-Serif", Font.ITALIC, 12));
+		constraints.weighty = 0.5;
+		constraints.gridx = 0;
+		constraints.gridy = 2;
+		pane.add(_commandLabel, constraints);
+
+		_commandField = new JTextField();
+		_commandField.setEditable(true);
+		constraints.weightx = 0.5;
+		constraints.weighty = 0.5;
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.gridx = 0;
+		constraints.gridy = 3;
+		constraints.gridwidth = 2;
+		pane.add(_commandField, constraints);
+
+		_submitButton = new JButton("Submit");
+		constraints.weightx = 0.5;
+		constraints.weighty = 0.5;
+		constraints.gridx = 1;
+		constraints.gridy = 3;
+		pane.add(_submitButton, constraints);
+
 		_commandField.addKeyListener(new KeyListener() {
 			@Override
-            public void keyTyped(KeyEvent arg0) {
-                // TODO Auto-generated method stub  
-            }
+			public void keyTyped(KeyEvent arg0) {
+				// TODO Auto-generated method stub  
+			}
 
-            @Override
-            public void keyReleased(KeyEvent arg0) {
-                // TODO Auto-generated method stub
-            }
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+			}
 
-            @Override
-            public void keyPressed(KeyEvent arg0) {
-            	if (arg0.getKeyCode() == KeyEvent.VK_ENTER){
-            		String userInput = _commandField.getText();
-            		
-                	if (userInput.toLowerCase().equals("exit")) {
-                		logger.log(Level.INFO, "System exit.");
-                		System.exit(0);
-                	} else {
-                		Response currentResponse = getLogic().processCommand(userInput);
-                		if (currentResponse.isSuccess()) {
-                			ArrayList<Entry> entries = currentResponse.getEntries();
-                			if (currentResponse.getFeedback() != null) {
-                				String feedback = currentResponse.getFeedback().trim();
-                				_feedbackArea.setText(feedback);
-                			}
-                			
-                			if (currentResponse.getEntries() != null) {
-	                			String entriesString = "";
-	                			for (int i = 0; i < entries.size(); i++) {
-	                				entriesString += entries.get(i).toString();
-	                				entriesString += "\n";
-	                			}
-	                			
-	                			_displayArea.setText(entriesString);
-                			}
-                			
-                			logger.log(Level.INFO, "Successfully updated display.");
-                		} else {
-                			String exception = currentResponse.getException().getMessage();
-                			_feedbackArea.setText(exception);
-                		}
-                	}
-                	
-                    _commandField.setText("");
-                }
-            }
-        });
-		
-		createLayout(new JComponent[]{_title, _displayScroll, _feedbackArea, _commandLabel, _commandField});
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				if (arg0.getKeyCode() == KeyEvent.VK_ENTER){
+					String userInput = _commandField.getText();
+
+					if (userInput.toLowerCase().equals("exit")) {
+						logger.log(Level.INFO, "System exit.");
+						System.exit(0);
+					} else {
+						Response currentResponse = getLogic().processCommand(userInput);
+						if (currentResponse.isSuccess()) {
+							ArrayList<Entry> entries = currentResponse.getEntries();
+							if (currentResponse.getFeedback() != null) {
+								String feedback = currentResponse.getFeedback().trim();
+								_feedbackArea.setText(feedback);
+							}
+
+							if (currentResponse.getEntries() != null) {
+								String entriesString = "";
+								for (int i = 0; i < entries.size(); i++) {
+									entriesString += entries.get(i).toString();
+									entriesString += "\n";
+								}
+
+								_displayArea.setText(entriesString);
+							}
+
+							logger.log(Level.INFO, "Successfully updated display.");
+						} else {
+							String exception = currentResponse.getException().getMessage();
+							_feedbackArea.setText(exception);
+						}
+					}
+
+					_commandField.setText("");
+				}
+			}
+		});
+
+		//createLayout(new JComponent[]{_title, _displayScroll, _feedbackArea, _commandLabel, _commandField});
 		_commandField.requestFocusInWindow();
-		
+
 		setTitle(TITLE);
-		//setSize(800, 600);
+		setSize(800, 600);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
-	private void createLayout(JComponent... arg) {
+
+	/*	private void createLayout(JComponent... arg) {
 
         Container pane = getContentPane();
         GroupLayout gl = new GroupLayout(pane);
         pane.setLayout(gl);
 
         gl.setAutoCreateContainerGaps(true);
-        
+
 	    gl.setHorizontalGroup(gl.createSequentialGroup()
 	    		.addGroup(gl.createParallelGroup()
 	    			.addComponent(arg[0])
@@ -132,7 +164,7 @@ public class UserInterface extends JFrame {
 	    			.addComponent(arg[4])
 		    	)
 	    );
-	
+
 	    gl.setVerticalGroup(gl.createSequentialGroup()
 	    		.addComponent(arg[0])
 	    		.addGap(10)
@@ -144,19 +176,26 @@ public class UserInterface extends JFrame {
 			    .addGap(10)
 			    .addComponent(arg[4])
 	    );
-        
+
 	    pack();
-    }
-	
+    }*/
+
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			@Override
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				logger.log(Level.INFO, "Program is running");
 				UserInterface _userInterface = new UserInterface();
+				_userInterface.pack();
 				_userInterface.setVisible(true);
 			}
 		});
 	}
-	
+	/*EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				logger.log(Level.INFO, "Program is running");
+				UserInterface _userInterface = new UserInterface();
+				_userInterface.pack();
+				_userInterface.setVisible(true);
+			}
+		});*/
 }
