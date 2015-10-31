c37e4d0c1037f5ef74acecdccdd8f4e9777ff5bb
Amarparkash Singh <nsjkapksm@gmail.com>
2015-10-31 12:23:39 +0800

    Added logging in IndexValidator class.

--- src/com/taskboard/main/IndexValidator.java
+++ src/com/taskboard/main/IndexValidator.java
@@ -1,11 +1,17 @@
 package com.taskboard.main;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class IndexValidator {
 	
 	private static final String MESSAGE_ERROR_FOR_INVALID_INDEX = "Invalid index provided.";
 	private static final int MIN_ENTRY_INDEX = 1;
+	
+	// attribute
+	
+	private Logger _logger;
 	
 	// constructor
 	
@@ -14,14 +20,12 @@ public class IndexValidator {
 	}
 	
 	public Response checkValidityOfInputIndex(String index, ArrayList<Entry> entries) {
-		Response responseForIndex = new Response();
-		
 		int indexValue = Integer.valueOf(index);
-			
 		int maxEntryIndex = entries.size();
-		
+		Response responseForIndex = new Response();
 		if (indexValue > maxEntryIndex || indexValue < MIN_ENTRY_INDEX) {
 			setFailureResponseForInvalidIndex(responseForIndex);
+			_logger.log(Level.INFO, "Generated failure response for invalid index");
 		} else {
 			responseForIndex.setIsSuccess(true);
 		}
