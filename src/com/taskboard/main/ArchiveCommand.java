a1e5d57c396814af99350bccc1c15fe680d59207
Amarparkash Singh <nsjkapksm@gmail.com>
2015-10-31 20:45:56 +0800

    Updated ArchiveCommand class and added logging.

--- src/com/taskboard/main/ArchiveCommand.java
+++ src/com/taskboard/main/ArchiveCommand.java
@@ -1,25 +1,29 @@
 package com.taskboard.main;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
 
 public class ArchiveCommand extends Command {
+	
+	private static final String MESSAGE_AFTER_ARCHIVE = "Successfully retrieved all archived entries.";  
 
 	public ArchiveCommand(ArrayList<Parameter> parameters) {
-			_parameters = parameters;
+		_parameters = parameters;
 		
 		if (getTempStorageManipulator() == null) {
 			_tempStorageManipulator = new TempStorageManipulator();
 		}
+		
+		_logger = GlobalLogger.getInstance().getLogger();
 	}
 	
 	public Response executeCommand() {
 		Response responseForArchive = new Response();
-		
 		responseForArchive.setIsSuccess(true);
-		responseForArchive.setFeedback("Successfully retrieved all archived entries.");
+		responseForArchive.setFeedback(MESSAGE_AFTER_ARCHIVE);
 		responseForArchive.setEntries(_tempStorageManipulator.getTempArchive());
+		_logger.log(Level.INFO, "Generated success response for accessing archive");
 		
 		return responseForArchive;
-	}
-	
+	}	
 }
