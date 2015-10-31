561897d67e383182c29e511386750e91ab146dac
Amarparkash Singh <nsjkapksm@gmail.com>
2015-10-31 22:25:43 +0800

    Updated LogicTest.

--- src/com/taskboard/test/LogicTest.java
+++ src/com/taskboard/test/LogicTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.io.IOException;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import com.taskboard.main.Logic;
 import com.taskboard.main.Command;
@@ -16,6 +17,7 @@ import com.taskboard.main.AddCommand;
 import com.taskboard.main.EditCommand;
 import com.taskboard.main.Response;
 import com.taskboard.main.Entry;
+import com.taskboard.main.DateComparator;
 import com.taskboard.main.Parameter;
 import com.taskboard.main.ParameterType;
 
@@ -40,20 +42,48 @@ public class LogicTest {
 		testParameters.clear();
 		Parameter nameParameter = new Parameter(ParameterType.NAME, "MA3264 Revision");
 		testParameters.add(nameParameter);
+		Parameter priorityParameter = new Parameter(ParameterType.PRIORITY, "low");
+		testParameters.add(priorityParameter);
 		command = new AddCommand(testParameters);
 		actualResponse = command.executeCommand();
 		
-		Entry entry = new Entry();
+		Entry floatingTask = new Entry();
 		Parameter indexParameter = new Parameter(ParameterType.INDEX, "1");
-		entry.addToParameters(indexParameter);
-		entry.addToParameters(nameParameter);
-		expectedEntries.add(entry);
+		floatingTask.addToParameters(indexParameter);
+		floatingTask.addToParameters(nameParameter);
+		floatingTask.addToParameters(priorityParameter);
+		expectedEntries.add(floatingTask);
 		String completedMessage = String.format(MESSAGE_AFTER_ADD, "MA3264 Revision");
 		expectedSuccessResponse = createSuccessResponse(true, completedMessage, expectedEntries);
-		testResponseEquality("Success response for adding floating task", expectedSuccessResponse, actualResponse);		
+		testResponseEquality("Success response for adding floating task with priority", expectedSuccessResponse,
+				             actualResponse);
+		
+		testParameters.clear();
+		nameParameter = new Parameter(ParameterType.NAME, "HW4 Assignment");
+		testParameters.add(nameParameter);
+		Parameter dateParameter = new Parameter(ParameterType.DATE, "04/11/2015");
+		testParameters.add(dateParameter);
+		Parameter timeParameter = new Parameter(ParameterType.TIME, "12:00");
+		testParameters.add(timeParameter);
+		Parameter categoryParameter = new Parameter(ParameterType.CATEGORY, "EE2021");
+		testParameters.add(categoryParameter);
+		command = new AddCommand(testParameters);
+		actualResponse = command.executeCommand();
+		
+		Entry deadlineTask = new Entry();
+		indexParameter = new Parameter(ParameterType.INDEX, "2");
+		deadlineTask.addToParameters(indexParameter);
+		deadlineTask.addToParameters(nameParameter);
+		deadlineTask.addToParameters(dateParameter);
+		deadlineTask.addToParameters(timeParameter);
+		deadlineTask.addToParameters(categoryParameter);
+		expectedEntries.add(deadlineTask);
+		Collections.sort(expectedEntries, new DateComparator());
+		completedMessage = String.format(MESSAGE_AFTER_ADD, "HW4 Assignment");
+		expectedSuccessResponse = createSuccessResponse(true, completedMessage, expectedEntries);
+		testResponseEquality("Success response for adding deadline task with category", expectedSuccessResponse,
+				             actualResponse);
 	}
-	
-	public void 
 	
 	private Response createSuccessResponse(boolean isSuccess, String feedback, ArrayList<Entry> entries) {
 		Response response = new Response();
