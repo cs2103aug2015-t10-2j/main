01586bfe189ceee7c7edf92acbaf64f3487f7e83
Amarparkash Singh <nsjkapksm@gmail.com>
2015-11-05 02:43:19 +0800

    Added author tag for collate to LogicTest.

--- src/com/taskboard/test/LogicTest.java
+++ src/com/taskboard/test/LogicTest.java
@@ -1,3 +1,4 @@
+//@@author A0123935E
 package com.taskboard.test;
 
 import static org.junit.Assert.assertEquals;
@@ -334,61 +335,4 @@ public class LogicTest {
 		testArchiveFileForOpen.delete();
 		testPreferenceFileForOpen.delete();
 	}
-	
-//	@Test
-//	public void testValidNewAndAdd() {
-//		ArrayList<Parameter> testParameters = new ArrayList<Parameter>();
-//		testParameters.add(new Parameter(ParameterType.NAME, "myFile"));
-//		Command command = new NewCommand(testParameters);
-//		Response actualResponse = command.executeCommand();
-//		
-//		ArrayList<Entry> expectedEntries = new ArrayList<Entry>();
-//		Response expectedSuccessResponse = createSuccessResponse(true, MESSAGE_WELCOME, expectedEntries);
-//		testResponseEquality("Success response for creating new file", expectedSuccessResponse, actualResponse);
-//		
-//		testParameters.clear();
-//		Parameter nameParameter = new Parameter(ParameterType.NAME, "MA3264 Revision");
-//		testParameters.add(nameParameter);
-//		Parameter priorityParameter = new Parameter(ParameterType.PRIORITY, "low");
-//		testParameters.add(priorityParameter);
-//		command = new AddCommand(testParameters);
-//		actualResponse = command.executeCommand();
-//		
-//		Entry floatingTask = new Entry();
-//		Parameter indexParameter = new Parameter(ParameterType.INDEX, "1");
-//		floatingTask.addToParameters(indexParameter);
-//		floatingTask.addToParameters(nameParameter);
-//		floatingTask.addToParameters(priorityParameter);
-//		expectedEntries.add(floatingTask);
-//		String completedMessage = String.format(MESSAGE_AFTER_ADD, "MA3264 Revision");
-//		expectedSuccessResponse = createSuccessResponse(true, completedMessage, expectedEntries);
-//		testResponseEquality("Success response for adding floating task with priority", expectedSuccessResponse,
-//				             actualResponse);
-//		
-//		testParameters.clear();
-//		nameParameter = new Parameter(ParameterType.NAME, "HW4 Assignment");
-//		testParameters.add(nameParameter);
-//		Parameter dateParameter = new Parameter(ParameterType.DATE, "04/11/2015");
-//		testParameters.add(dateParameter);
-//		Parameter timeParameter = new Parameter(ParameterType.TIME, "12:00");
-//		testParameters.add(timeParameter);
-//		Parameter categoryParameter = new Parameter(ParameterType.CATEGORY, "EE2021");
-//		testParameters.add(categoryParameter);
-//		command = new AddCommand(testParameters);
-//		actualResponse = command.executeCommand();
-//		
-//		Entry deadlineTask = new Entry();
-//		indexParameter = new Parameter(ParameterType.INDEX, "2");
-//		deadlineTask.addToParameters(indexParameter);
-//		deadlineTask.addToParameters(nameParameter);
-//		deadlineTask.addToParameters(dateParameter);
-//		deadlineTask.addToParameters(timeParameter);
-//		deadlineTask.addToParameters(categoryParameter);
-//		expectedEntries.add(deadlineTask);
-//		Collections.sort(expectedEntries, new DateComparator());
-//		completedMessage = String.format(MESSAGE_AFTER_ADD, "HW4 Assignment");
-//		expectedSuccessResponse = createSuccessResponse(true, completedMessage, expectedEntries);
-//		testResponseEquality("Success response for adding deadline task with category", expectedSuccessResponse,
-//				             actualResponse);
-//	}
 }
