f1944cd387798378eae3d318afa6cc3d9352c318
Alvian Prasetya <alvian.better@gmail.com>
2015-11-04 18:49:23 +0800

    Changed default background and renamed background files.

--- src/com/taskboard/main/UserInterface.java
+++ src/com/taskboard/main/UserInterface.java
@@ -33,7 +33,7 @@ public class UserInterface extends JFrame {
 	private static final String NORMAL_PRIORITY_FILE_PATH = "resources/images/NormalPriority.jpg";
 	private static final String PAST_ENTRY_FILE_PATH = "resources/images/Past.png";
 	private static final String REMINDER_FILE_PATH = "resources/images/Soon.png";
-	private static final String DEFAULT_BACKGROUND_FILE_PATH = "resources/images/cool-backround-279.jpg";
+	private static final String DEFAULT_BACKGROUND_FILE_PATH = "resources/images/default.jpg";
 	private static final int DEFAULT_REMINDER_HOUR = 1;
 	
 	private static final String MESSAGE_PROMPT_COMMAND = "Enter command below:";
